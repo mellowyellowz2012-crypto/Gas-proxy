15f1f5b49298cc62f8eaf97094a0620a013eff1a
mellowyellowz2012-crypto <mellowyellowz2012@gmail.com>
2026-05-09 15:53:13 -0400

    Create gsa.js

---
+++ api/gsa.js
@@ -0,0 +1,9 @@
+export default async function handler(req, res) {
+  const { city, state } = req.query;
+  const apiKey = 'YOUR_GSA_KEY';
+  const url = `https://api.gsa.gov/travel/perdiem/v2/rates/city/${city}/state/${state}/year/2025?api_key=${apiKey}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.status(200).json(data);
+}
